48650f5946a0c46c78eb71417528a705d3b9db74
FerdousFardin <fardinh39u@gmaile.com>
2022-10-25 19:06:12 +0600

    dummy json files with id and category id for courses.json added

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require("./data/category.json");
+const courses = require("./data/courses.json");
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(courses.length);
+  const selectedCategory = courses.filter(
+    (course) => id === course.category_id
+  );
+  res.send(selectedCategory);
+});
 app.listen(port, () => {
   console.log("Server is now listening this port:", port);
 });
